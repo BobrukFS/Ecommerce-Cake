6c7594f65b8f0a345a5943e4cd91bb97032dcd73
Exequiel Bobruk <bobrukfs@gmail.com>
2023-09-30 02:06:00 -0300

    feat:cambio header y agrego seccion about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,18 +3,25 @@ import React from 'react'
 function About() {
   return (
     <>
-    <div className="about container-fluid p-md-4 p-lg-5">
-        <div>
-            <h2 className='fs-1 titles-font text-secondary fw-bold'>Sobre nosotros</h2>
-            <p className='paragraph-font fs-5'>Nuestro objetivo es ofrecer manualidades exquisitas y originales que hagan que tus momentos sean inolvidables</p>
+    <div className="about container-fluid p-md-4 p-lg-5 ">
+        <div className='about__desc d-flex flex-column justify-content-end p-3'>
+          <div className='px-3 px-lg-5'>
+      
+            <h2 className='fs-1 titles-font text-white fw-bold border  border-3 border-secondary border-top-0 border-bottom-0 border-end-0  px-3'>Sobre nosotros</h2>
+            <p className='paragraph-font fs-5 text-white'>Nuestro objetivo es ofrecer manualidades exquisitas y originales que hagan que tus momentos sean inolvidables.</p>
+          
+            </div>
         </div>
-        <div className='row d-flex align-items-start'>
-            <img className='my-4 my-lg-0 col-lg-6' src="./about.png" alt="" />
-            <div className='col-lg-6 py-4'>
+        <div className='about__main  row d-flex '>
+          <div className=' justify-content-center col-md-5 d-flex gap-4'>
+            <img className='about__img  ' src="./about2.png" alt="" />
+            <img className='about__img  ' src="./about3.png" alt="" />       
+          </div>
+            <div className='col-md-5 py-1 mb-5 '>
  
-                <p className='paragraph-font fs-5'>En Tiempo de Fantasia, nos especializamos en ofrecer manualidades personalizadas para una amplia variedad de eventos.</p>
-                <p className='paragraph-font fs-5'>Ya sea un cumpleaños, un bautismo, un casamiento o comunion, nos esforzamos por crear piezas unicas y significativas que se adapten perfectamente a cada ocasion. Nos complace ser parte de tus momentos mas especiales y convertirlos en recuerdos eternos.</p>
-                <a className=" w-100 bg-primary position-relative  align-self-start intro__btn btn1 text-danger border border-danger fw-bold titles-font rounded-pill fs-5">¡Visitar tienda!</a>
+                <p className='paragraph-font my-2 fs-6'>En Tiempo de Fantasia, nos especializamos en ofrecer manualidades personalizadas para una amplia variedad de eventos.</p>
+                <p className='paragraph-font fs-6 mb-4'>Ya sea un cumpleaños, un bautismo, un casamiento o comunion, nos esforzamos por crear piezas unicas y significativas que se adapten perfectamente a cada ocasion. Nos complace ser parte de tus momentos mas especiales y convertirlos en recuerdos eternos.</p>
+                <a  className="w-100 position-relative bg-primary  align-self-start intro__btn btn1 text-danger border border-danger fw-bold titles-font rounded-pill fs-6  px-sm-4  px-lg-5">¡Visitar tienda!</a>
             </div>
         </div>
         
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -18,8 +18,8 @@ function Header() {
 
   return (
     <>
-        <div className='header container-fluid d-flex w-100 align-items-center justify-content-between p-2 position-fixed   top-0 start-0'>
-        <p className='fs-1 cursiva ms-1'>Sweet<span className='text-terciary'>Cake</span></p>
+        <div className=' px-4 header container-fluid d-flex w-100 align-items-center justify-content-between p-2 position-absolute   top-0 start-0 px-lg-5'>
+        <p className='fs-1 cursiva ms-1 my-0'>Sweet<span className='text-terciary'>Cake</span></p>
         <div className="box z-3 d-md-none">
                 <div className="btn1  not-active d-block d-lg-none bg-primary border border-danger position-relative" onClick={abrirMenu}>
                   <span></span>
@@ -27,12 +27,17 @@ function Header() {
                   <span></span>
                 </div>
         </div>
-            <nav className=' header__nav z-2 d-flex flex-column flex-md-row  justify-content-center justify-content-md-end align-items-center h-100 w-100 position-lg-relative top-0 start-0  '>
-            <Link className=' text-decoration-none mb-4 mb-md-0 mx-md-3 fs-3 paragraph-font' to="/">Inicio</Link>
+            <nav className=' header__nav z-2 d-flex flex-column flex-md-row  justify-content-center justify-content-md-between align-items-center h-100 w-100 position-lg-relative top-0 start-0  '>
+              <div className='d-flex flex-column flex-md-row mx-4'>
                 <Link className=' text-decoration-none mb-4 mb-md-0 mx-md-3 fs-3 paragraph-font' to="/about">Nosotros</Link>
                 <Link className=' text-decoration-none mb-4 mb-md-0 mx-md-3 fs-3 paragraph-font' to="/contact">Contacto</Link>
                 <Link className=' text-decoration-none mb-4 mb-md-0 mx-md-3 fs-3 paragraph-font' to="/Tienda">Tienda</Link>
+              </div>
+                <div className='d-flex flex-column flex-md-row align-items-center'>
+                <Link className=' text-decoration-none mb-4 mb-md-0 mx-md-3 fs-3 paragraph-font' to="/">Iniciar Sesion</Link>
                 <Link className="carrito position-relative d-flex bg-primary border border-danger"><i className="bi bi-cart3 text-white fs-1"></i></Link>
+                </div>
+                
             </nav>
         </div>
 
